764289cd97d9cd396415e2a98e494b69c2942ef8
Micah Evans <micah.s.evans@gmail.com>
2020-01-18 12:18:22 -0500

    CODE: Added Ajax calls and for loops for Beef.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
 
 
-  drink = "Vodka"
-  // var qURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drink
-  var qURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+  drink = $("#").val();
+  var qURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drink
+  // var qURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+
 
   $.ajax({
     url: qURL,
@@ -11,6 +12,24 @@ $(document).ready(function () {
   }).then(function (response) {
     console.log(response);
     console.log(qURL);
-  });
 
-});
+    if (drink === "Beef") {
+      var allRum = response.drinks
+      var drinkID = respose.drinks.idDrink
+      var drinkIMG = response.drinks.strDrinkThumb
+      $.ajax({
+        url: https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=rum,
+        method: "GET"
+      })(function (response) {
+        console.log(response.drinks);
+        for (let index = 0; index < drinks.length; index++) {
+          console.log(response);
+
+        }
+
+
+
+      });
+
+
+    });
