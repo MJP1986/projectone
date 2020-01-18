e7656512224cfcc322a235b67b0f057b7078687b
Micah Evans <micah.s.evans@gmail.com>
2020-01-18 10:57:51 -0500

    CODE: I've begun initial console log testing on the drinks db.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,16 @@
-drink = "Vodka"
-var qURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drink
+$(document).ready(function () {
 
 
-$.ajax({
+  drink = "Vodka"
+  // var qURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drink
+  var qURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+
+  $.ajax({
     url: qURL,
     method: "GET"
-  }).then(function(response) {
+  }).then(function (response) {
     console.log(response);
+    console.log(qURL);
   });
+
+});
