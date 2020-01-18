a126493a3c7caeb2b92e6f075abb2e7b8edf3880
Jack Markwalter <jackmarkwalter3@gmail.com>
2020-01-18 12:18:04 -0500

    created food arrays

--- javascript.js
+++ javascript.js
@@ -10,21 +10,33 @@ var time = "30+"
 
 var qURLFirst = "https://api.spoonacular.com/recipes/search?query=" + query + "&excludeIngredients=" + excludeIngredients + "&intolerances=" + intolerances + "&diet=" + diet + "&number=10&apiKey=874efe7482a6472e93947420655c6cad";
 
+//Need the image, title, link
+
 
 $.ajax({
     url: qURLFirst,
     method: "GET"
-  }).then(function(response2) {
-    console.log(response2);
-    var recipeID = response2.results[0].id;
+  }).then(function(response) {
+    console.log(response);
+    var recipeIDArray = [];
+    var foodImgArray = [];
+    var foodTitleArray = [];
+    var foodLinkArray = [];
+    for (var i = 0; i < response.results.length; i++) {
+        var recipeID = response.results[i].id;
+        var foodTitle = response.results[i].title;
+        var foodImg = "https://spoonacular.com/recipeImages" + response.results[i].image;
 
-
-    qURLThird = "https://api.spoonacular.com/recipes/" + recipeID + "/information?includeNutrition=false&apiKey=874efe7482a6472e93947420655c6cad";
-
-    $.ajax({
-        url: qURLSecond,
-        method: "GET"
-    }).then(function(response3) {
-        console.log(response3);
-    })
+        recipeIDArray.push(recipeID);
+        recipeIDArray.push(foodTitle);
+        recipeIDArray.push(foodImg);
+        qURLThird = "https://api.spoonacular.com/recipes/" + recipeID + "/information?includeNutrition=false&apiKey=874efe7482a6472e93947420655c6cad";
+        $.ajax({
+            url: qURLSecond,
+            method: "GET"
+        }).then(function(response2) {
+            console.log(response2);
+    
+        })
+    }
   });
