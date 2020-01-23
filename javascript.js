3c49ba1e3a2fd0d8de754377187b32e3fb77dcd2
Micah Evans <micah.s.evans@gmail.com>
2020-01-22 19:11:42 -0500

    I've finished the JS Drinks code and pasted it all back into the original javascript.js file. We should be good to go.

--- javascript.js
+++ javascript.js
@@ -51,173 +51,185 @@
 
 
 // // MEvans JS - JS that manages the drink calls
-// $(document).ready(function () {
+$(document).ready(function () {
 
-//   var drink = 0
+  var drink = 0
 
-//   var Meats = "Pork"
-//   // var Meats = $("#").val()
+  var Meats = "Pork"
+  // var Meats = $("#").val()
 
-//   // These calls set the drink results based on the meat choice
-//   function setDrink() {
-//     if (Meats === "Beef") {
-//       drink = "rum";
-//       console.log(Meats);
-//     }
-//     else if (Meats === "Pork") {
-//       drink = "gin";
-//       console.log(Meats);
-//     }
-//     else if (Meats === "Veggie") {
-//       drink = "wine";
-//       console.log(Meats);
-//     }
-//     else if (Meats === "Chicken") {
-//       drink = "brandy";
-//       console.log(Meats);
-//     }
-//     else if (Meats === "Shellfish") {
-//       drink = "tequila";
-//       console.log(Meats);
-//     }
-//     else if (Meats === "fish") {
-//       drink = "vodka";
-//       console.log(Meats);
-//     }
+  // These calls set the drink results based on the meat choice
+  function setDrink() {
+    if (Meats === "Beef") {
+      drink = "rum";
+      console.log(Meats);
+    }
+    else if (Meats === "Pork") {
+      drink = "gin";
+      console.log(Meats);
+    }
+    else if (Meats === "Veggie") {
+      drink = "wine";
+      console.log(Meats);
+    }
+    else if (Meats === "Chicken") {
+      drink = "brandy";
+      console.log(Meats);
+    }
+    else if (Meats === "Shellfish") {
+      drink = "tequila";
+      console.log(Meats);
+    }
+    else if (Meats === "fish") {
+      drink = "vodka";
+      console.log(Meats);
+    }
 
-//   }
-//   setDrink();
-
-
-
-//   var qURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drink;
-//   // var qURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
-
-//   // Call that randomizes the drink results based on the above Meat entry by the user.
-//   $.ajax({
-//     url: qURL,
-//     method: "GET"
-//   }).then(function (response) {
-//     // console.log(response);
-//     // console.log(qURL);
-
-//     var drinkList = response.drinks
-//     // console.log(response.drinks);
-
-//     var drinkID1 = response.drinks[Math.ceil(Math.random() * drinkList.length)];
-//     var drinkID2 = response.drinks[Math.ceil(Math.random() * drinkList.length)];
-//     var drinkID3 = response.drinks[Math.ceil(Math.random() * drinkList.length)];
-
-//     console.log(drinkID1);
-//     console.log(drinkID2);
-//     console.log(drinkID3);
-
-//     // Call that pulls the ID from the above randomized drinks - needed in order to pull the ingredients & recipe list from the drink API
+  }
+  setDrink();
 
 
-//     // $.ajax({
-//     //   url: qURL,
-//     //   method: "GET"
-//     // }).then(function (response) {
-//     //   console.log(response);
-//     //   console.log(qURL);
 
-//       drinkResult1 = drinkID1.idDrink;
-//       drinkResult2 = drinkID2.idDrink;
-//       drinkResult3 = drinkID3.idDrink;
+  var qURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drink;
+  // var qURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
 
-//       console.log(drinkResult1);
-//       console.log(drinkResult2);
-//       console.log(drinkResult3);
+  // Call that randomizes the drink results based on the above Meat entry by the user.
 
-//       // This call pulls the ingredient list from the first randomized drink result above
-//       var qURLdrinkResult1 = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkResult1;
+  $.ajax({
+    type: "post",
+    url: qURL,
+    method: "GET",
+    success: function (response) {
 
-//       $.ajax({
-//         url: qURLdrinkResult1,
-//         method: "GET"
-//       }).then(function (response) {
-//         // console.log(response);
-//         // console.log(qURLdrinkResult1);
+      var drinkList = response.drinks
 
-//         var drinkDisplay1 = (response.drinks[0].strIngredient1 + ", " +
-//           response.drinks[0].strIngredient2 + ", " +
-//           response.drinks[0].strIngredient3 + ", " +
-//           response.drinks[0].strIngredient4 + ", " +
-//           response.drinks[0].strIngredient5 + ", " +
-//           response.drinks[0].strIngredient6 + ", " +
-//           response.drinks[0].strIngredient7 + ", " +
-//           response.drinks[0].strIngredient8 + ", " +
-//           response.drinks[0].strIngredient9 + ", " +
-//           response.drinks[0].strIngredient10);
+      var drinkID1 = response.drinks[Math.ceil(Math.random() * drinkList.length)];
+      var drinkID2 = response.drinks[Math.ceil(Math.random() * drinkList.length)];
+      var drinkID3 = response.drinks[Math.ceil(Math.random() * drinkList.length)];
 
-//         console.log(drinkDisplay1);
+      console.log(drinkID1);
+      console.log(drinkID2);
+      console.log(drinkID3);
 
-//         // This call pulls the drink instructions for the first drink
-//         var drinkIn1 = response.drinks[0].strInstructions
-//         console.log(drinkIn1);
+      // Call that pulls the ID from the above randomized drinks - needed in order to pull the ingredients & recipe list from the drink API
 
-//         // This call pulls the second ingredient list
-//         var qURLdrinkResult2 = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkResult2;
+      $.ajax({
+        type: "post",
+        url: qURL,
+        method: "GET",
+        success: function (response) {
 
-//         $.ajax({
-//           url: qURLdrinkResult2,
-//           method: "GET"
-//         }).done(function (response) {
-//           // console.log(response);
-//           // console.log(qURLdrinkResult2);
+          drinkResult1 = drinkID1.idDrink;
+          drinkResult2 = drinkID2.idDrink;
+          drinkResult3 = drinkID3.idDrink;
 
-//           var drinkDisplay2 = (response.drinks[0].strIngredient1 + ", " +
-//             response.drinks[0].strIngredient2 + ", " +
-//             response.drinks[0].strIngredient3 + ", " +
-//             response.drinks[0].strIngredient4 + ", " +
-//             response.drinks[0].strIngredient5 + ", " +
-//             response.drinks[0].strIngredient6 + ", " +
-//             response.drinks[0].strIngredient7 + ", " +
-//             response.drinks[0].strIngredient8 + ", " +
-//             response.drinks[0].strIngredient9 + ", " +
-//             response.drinks[0].strIngredient10);
+          console.log(drinkResult1);
+          console.log(drinkResult2);
+          console.log(drinkResult3);
 
-//           console.log(drinkDisplay2);
+          // This call pulls the ingredient list from the first randomized drink result above
 
-//           // This call pulls the drink instructions for the first drink
-//           var drinkIn2 = response.drinks[0].strInstructions
-//           console.log(drinkIn2);
+          var qURLdrinkResult1 = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkResult1;
 
-//         });
+          $.ajax({
+            type: "post",
+            url: qURLdrinkResult1,
+            method: "GET",
+            success: function (response) {
 
-//         // This call pulls the third ingredient list
-//         var qURLdrinkResult3 = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkResult3;
+              var drinkDisplay1 = (response.drinks[0].strIngredient1 + ", " +
+                response.drinks[0].strIngredient2 + ", " +
+                response.drinks[0].strIngredient3 + ", " +
+                response.drinks[0].strIngredient4 + ", " +
+                response.drinks[0].strIngredient5 + ", " +
+                response.drinks[0].strIngredient6 + ", " +
+                response.drinks[0].strIngredient7 + ", " +
+                response.drinks[0].strIngredient8 + ", " +
+                response.drinks[0].strIngredient9 + ", " +
+                response.drinks[0].strIngredient10);
 
-//         $.ajax({
-//           url: qURLdrinkResult3,
-//           method: "GET"
-//         }).done(function (response) {
-//           // console.log(response);
-//           // console.log(qURLdrinkResult3);
+              console.log(drinkDisplay1);
+              var drinkIn1 = response.drinks[0].strInstructions
+              console.log(drinkIn1);
 
-//           var drinkDisplay3 = (response.drinks[0].strIngredient1 + ", " +
-//             response.drinks[0].strIngredient2 + ", " +
-//             response.drinks[0].strIngredient3 + ", " +
-//             response.drinks[0].strIngredient4 + ", " +
-//             response.drinks[0].strIngredient5 + ", " +
-//             response.drinks[0].strIngredient6 + ", " +
-//             response.drinks[0].strIngredient7 + ", " +
-//             response.drinks[0].strIngredient8 + ", " +
-//             response.drinks[0].strIngredient9 + ", " +
-//             response.drinks[0].strIngredient10);
+              // This call pulls the ingredient list from the second randomized drink result above
 
-//           console.log(drinkDisplay3);
+              var qURLdrinkResult2 = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkResult2;
 
-//           var drinkIn3 = response.drinks[0].strInstructions
-//           console.log(drinkIn3);
+              $.ajax({
+                type: "post",
+                url: qURLdrinkResult2,
+                method: "GET",
+                success: function (response) {
 
-//         });
+                  var drinkDisplay2 = (response.drinks[0].strIngredient1 + ", " +
+                    response.drinks[0].strIngredient2 + ", " +
+                    response.drinks[0].strIngredient3 + ", " +
+                    response.drinks[0].strIngredient4 + ", " +
+                    response.drinks[0].strIngredient5 + ", " +
+                    response.drinks[0].strIngredient6 + ", " +
+                    response.drinks[0].strIngredient7 + ", " +
+                    response.drinks[0].strIngredient8 + ", " +
+                    response.drinks[0].strIngredient9 + ", " +
+                    response.drinks[0].strIngredient10);
 
-//       });
+                  console.log(drinkDisplay2);
 
-//     // });
+                  // This call pulls the drink instructions for the first drink
+                  var drinkIn2 = response.drinks[0].strInstructions
+                  console.log(drinkIn2);
 
-//   })
 
-// });
+                  // This call pulls the ingredient list from the second randomized drink result above
+
+                  var qURLdrinkResult3 = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkResult3;
+
+                  $.ajax({
+                    type: "post",
+                    url: qURLdrinkResult3,
+                    method: "GET",
+                    success: function (response) {
+
+                      var drinkDisplay3 = (response.drinks[0].strIngredient1 + ", " +
+                        response.drinks[0].strIngredient2 + ", " +
+                        response.drinks[0].strIngredient3 + ", " +
+                        response.drinks[0].strIngredient4 + ", " +
+                        response.drinks[0].strIngredient5 + ", " +
+                        response.drinks[0].strIngredient6 + ", " +
+                        response.drinks[0].strIngredient7 + ", " +
+                        response.drinks[0].strIngredient8 + ", " +
+                        response.drinks[0].strIngredient9 + ", " +
+                        response.drinks[0].strIngredient10);
+
+                      console.log(drinkDisplay3);
+
+                      // This call pulls the drink instructions for the first drink
+                      var drinkIn3 = response.drinks[0].strInstructions
+                      console.log(drinkIn3);
+
+
+
+                    }
+
+                  })
+                }
+
+              })
+
+            }
+
+          })
+
+        }
+
+
+
+      });
+
+    }
+
+
+
+  });
+
+});
